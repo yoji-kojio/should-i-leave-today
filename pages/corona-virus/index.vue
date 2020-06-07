d6bd18ce7545daec3ae7e637391e5ffd24c523b0
Yoji Kojio <yoji.kojio@gmail.com>
2020-06-06 21:14:40 -0300

    Create corona files

---
+++ pages/corona-virus/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      Corona Virus
+    </h1>
+    <h2 class="subtitle">
+      Should I leave my house today?
+    </h2>
+  </div>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+</style>
